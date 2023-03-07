d7cb92413771a9d4c940ea186531c01023125a48
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2023-03-07 17:45:04 -0300

    feat: iniciate page

--- src/backend/app.py
+++ src/backend/app.py
@@ -4,11 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('iniciate.html')
 
 @app.route('/status')
 def conection():
     return render_template('status.html')
-
 
 app.run(host = '0.0.0.0', port=3000, debug=True)
