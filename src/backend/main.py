0b7ce0819c446f8b8fd25fa428ef33d0ec873354
HallZero <filipikikuchi@gmail.com>
2023-03-07 17:33:16 -0300

    feat(server): initial integration between parts

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from Dobot import Dobot
 
 app = Flask(__name__)
 
@@ -9,6 +10,28 @@ def index():
 @app.route('/status')
 def conection():
     return render_template('status.html')
+
+@app.route('/routine')
+def routine():
+
+    arm = Dobot(225, 3, 140, 0)
+
+    arm.moveHome()
+
+    arm.pickToggle()
+    arm.moveArmXY(189, 183, 151, 41)
+    arm.drawLine(200, 183, -10, 41, -150)
+    arm.moveArmXY(189, 183, 151, 41)
+    arm.moveHome()
+    arm.drawLine(302, 0, -10, 0, -100)
+    arm.rotateTool(90)
+    arm.moveHome()
+    arm.moveArmXY(128, -195, 14, 0)
+    # arm.pickToggle()
+
+    arm.disconnect()
+    return "FODA MUITO FODA!"
+
 
 
 app.run(host = '0.0.0.0', port=3000, debug=True)
