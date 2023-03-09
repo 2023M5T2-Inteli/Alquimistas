cd7b0246c5befe5219921ba55588e17f53c82b84
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2023-03-09 17:16:27 -0300

    feat: last page

--- src/backend/app.py
+++ src/backend/app.py
@@ -10,4 +10,12 @@ def index():
 def conection():
     return render_template('status.html')
 
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+@app.route('/end')
+def end():
+    return render_template('end.html')
+
 app.run(host = '0.0.0.0', port=3000, debug=True)
