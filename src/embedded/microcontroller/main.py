2ce9e473c50e9b2c59520ed1d16cdc0337101711
Gabriela Rodrigues <99189378+gabInteli@users.noreply.github.com>
2023-03-09 02:04:04 -0300

    update microcontroller main.py

--- src/embedded/microcontroller/main.py
+++ src/embedded/microcontroller/main.py
@@ -2,7 +2,7 @@ from machine import Pin
 from time import sleep
 
 class Componentes:
-    def _init_(self, in1_pin, in2_pin, in3_pin, in4_pin, button1_pin, button2_pin):
+    def __init__(self, in1_pin, in2_pin, in3_pin, in4_pin, button1_pin, button2_pin):
         self.IN1 = Pin(in1_pin, Pin.OUT)
         self.IN2 = Pin(in2_pin, Pin.OUT)
         self.IN3 = Pin(in3_pin, Pin.OUT)
@@ -15,17 +15,14 @@ class Componentes:
             self.IN1.high()
             self.IN2.low()
             print("Componentes ligados")
-        elif self.Button2.value() == 1:
-            self.IN1.low()
-            self.IN2.high()
-            print("Componentes ligados")
         else:
             self.IN1.low()
             self.IN2.low()
             print("Componentes desligados")
 
-componentes = Componentes(0, 1, 2, 3, 16, 17)
+componentes = Componentes(0, 1, 4, 5, 15, 17)
 
 while True:
     sleep(2)
     componentes.ligar_componentes()
+    print(Pin(15).value())
