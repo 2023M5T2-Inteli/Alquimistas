5016e5b0abcd77025a4a65337a2256a0daf88523
Gabriela Rodrigues <gabriela.matias@sou.inteli.edu.br>
2023-04-03 15:25:17 -0300

    update db structure

--- src/backend/models/report.py
+++ src/backend/models/report.py
@@ -1,9 +1,10 @@
 from models.base import Base
-from sqlalchemy import Column, Integer, String, Boolean, Float
+from sqlalchemy import Column, Integer, String, Boolean, Float, DateTime
 
 class Report(Base):
     __tablename__ = "Reports"
     id = Column(Integer, primary_key=True)
+    date = Column(DateTime(timezone=True))
     project=Column(String)
     client=Column(String)
     sample=Column(String)
@@ -11,6 +12,11 @@ class Report(Base):
     cycles=Column(Integer)
     liquid_initial_mass=Column(Float)
     solid_initial_mass=Column(Float)
+    duration = Column(Float)
+    magnetic_field = Column(Float)
+    velocity = Column(Float)
+    cycle_time=Column(Float)
+    magnet_heigth = Column(Float)
 
     def __repr__(self) -> str:
       return f"Report(id={self.id}, client={self.client}, sample={self.sample}, operator={self.operator}, cycles={self.cycles}, liquid_initial_mass={self.liquid_initial_mass}, solid_initial_mass={self.solid_initial_mass})"
